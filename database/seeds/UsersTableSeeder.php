8ae90ac80479a739c8ca3f0c8c59ba011d77286a
alfredobarron <alfredobarronc@gmail.com>
2018-09-19 13:08:51 -0500

    add Laravel auth, and create UsersTableSeeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -2,15 +2,15 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UsersTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
+        factory(App\User::class, 50)->create();
     }
 }
