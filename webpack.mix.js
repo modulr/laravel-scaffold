209e52fb985876593cac32dafbbd34e87255bd82
alfredobarron <alfredobarronc@gmail.com>
2018-11-22 00:00:31 -0600

    change cache views

--- webpack.mix.js
+++ webpack.mix.js
@@ -28,8 +28,9 @@ mix.webpackConfig({
        handleFetch: true,
        dynamicUrlToDependencies: { //you should add the path to your blade files here so they can be cached
               //and have full support for offline first (example below)
-           '/': ['resources/views/layouts/auth.blade.php'],
-           '/': ['resources/views/layouts/app.blade.php'],
+           '/': ['resources/views/welcome.blade.php'],
+           //'/': ['resources/views/layouts/auth.blade.php'],
+           //'/': ['resources/views/layouts/app.blade.php'],
            // '/posts': ['resources/views/posts.blade.php']
        },
        staticFileGlobsIgnorePatterns: [/\.map$/, /mix-manifest\.json$/, /manifest\.json$/, /service-worker\.js$/],
