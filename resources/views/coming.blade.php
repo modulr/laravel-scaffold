05dc14cda280bda5463d9157ab6e030a5ad5fd40
alfredobarron <alfredobarronc@gmail.com>
2019-01-22 23:30:26 -0600

    add button style

--- resources/views/coming.blade.php
+++ resources/views/coming.blade.php
@@ -4,22 +4,38 @@
 <div class="row">
     <div class="col-sm-12 text-center">
         <img class="img-fluid logo" src="/img/sisi_slogan.png" alt="">
-        <br><br><br>
-        <br><br><br>
+        <br><br><br><br>
     </div>
 
     <div class="col-sm-12 text-center">
-        <a href="tel:627-142-0604"><h1><i class="fas fa-phone"></i> 627 142 0604</h1></a>
+        <p>Llámanos</p>
+        <a class="btn-white btn-phone" href="tel:627-142-0604">
+            <h3>
+                <!-- <i class="fas fa-phone"></i> -->
+                <small class="fa-stack">
+                  <i class="fas fa-circle fa-stack-2x"></i>
+                  <i class="fas fa-phone fa-stack-1x fa-inverse"></i>
+              </small>
+                 627 142 0604
+            </h3>
+        </a>
         <br>
     </div>
 
     <div class="col-sm-12 text-center">
-        <a href="https://api.whatsapp.com/send?phone=526271420604&text=Me%20puedes%20traer%20&source=&data="><h3><i class="fab fa-whatsapp wp-color"></i> WhatsApp</h3></a>
-        <br><br><br>
+        <p>o haz tu pedido por</p>
+        <a class="btn btn-lg btn-light" href="https://api.whatsapp.com/send?phone=526271420604&text=Me%20puedes%20traer%20&source=&data=">
+            <i class="fab fa-whatsapp"></i> WhatsApp
+        </a>
+        <br><br><br><br><br>
     </div>
 
     <div class="col-sm-12 text-center">
-        <p><a href="mailto:megusta@sisiapp.mx">megusta@sisiapp.mx</a></p>
+        <p>
+            <a class="btn-white" href="mailto:megusta@sisiapp.mx">
+            megusta@sisiapp.mx
+        </a>
+        </p>
     </div>
 </div>
 @endsection
