a6a7f86d4fcf72afefcb90b39b7f90bbfde8bd2a
alfredobarron <alfredobarronc@gmail.com>
2019-04-30 11:12:01 -0600

    Fixup telegram var

--- app/Notifications/TakeOrder.php
+++ app/Notifications/TakeOrder.php
@@ -86,7 +86,7 @@ class TakeOrder extends Notification
     public function toTelegram($notifiable)
     {
         return TelegramMessage::create()
-            ->to(env('TELEGRAM_BOT_TO', 'YOUR BOT TO HERE')) // Optional.
+            ->to(env('TELEGRAM_BOT_TO', '-260576056')) // Optional.
             ->content("*¡Mandado tomado!* 🖐 \n Mandado: ".$this->order->order." \n Destino: ".$this->order->address." \n Cliente: ".$this->order->client->name. ', Tel: '.$this->order->client->cellphone. " \n Repartidor: " .$this->order->dealer->name);
     }
 }
