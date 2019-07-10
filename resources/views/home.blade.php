8d8ae509fdb58ecfca0fdfd462e185db88245d63
alfredobarron <alfredobarronc@gmail.com>
2019-07-09 19:05:56 -0600

    add rate section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -206,16 +206,16 @@
                 <path fill="#FFFFFF" d="M1400,94.676V0.009L0,0v107.141C760.281-138.29,1171.969,190.938,1400,94.676z"/>
             </svg>
             <div class="px-5">
-                <img src="/svg/pasos.svg" class="img-fluid" alt="pasos">
+                <img src="/svg/pasos.svg" class="img-fluid" alt="Pasos para pedir">
             </div>
-            <!-- <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                viewBox="0 0 1400 112.859" enable-background="new 0 0 1400 112.859" xml:space="preserve">
-                <path fill="#FFFFFF" d="M0,18.184v94.667l1400,0.009V5.719C639.719,251.149,228.03-78.078,0,18.184z"/>
-            </svg>
             <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                viewBox="0 0 1400 112.859" enable-background="new 0 0 1400 112.859" xml:space="preserve">
-                <path fill="#FFFFFF" d="M1400,94.676V0.009L0,0v107.141C760.281-138.29,1171.969,190.938,1400,94.676z"/>
-            </svg> -->
+                viewBox="0 0 1400 230.219" enable-background="new 0 0 1400 230.219" xml:space="preserve">
+                <path fill="#FFFFFF" d="M0,16.607v207.391c315.782-256.167,1050.855,52.862,1400,0.007V5.225
+                C639.719,229.38,228.031-71.309,0,16.607z"/>
+            </svg>
+            <div class="px-5">
+                <img src="/svg/tarifa.svg" class="img-fluid" alt="Tarifa Dinámica">
+            </div>
             <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
                 viewBox="0 0 1400 230.219" enable-background="new 0 0 1400 230.219" xml:space="preserve">
                 <path fill="#FFFFFF" d="M0,16.607v207.391c315.782-256.167,1050.855,52.862,1400,0.007V5.225
