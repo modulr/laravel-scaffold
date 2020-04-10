175976a1dc3a15da898a5bb169ff972eb709311e
alfredobarron <alfredobarronc@gmail.com>
2020-04-10 18:31:29 -0500

    fixup whatsapp number

--- resources/js/components/layout/Config.vue
+++ resources/js/components/layout/Config.vue
@@ -6,7 +6,7 @@
       <a :href="this.config.instagram" target="_blank" class="text-secondary mr-3">
         <i class="fab fa-instagram fa-2x"></i>
       </a>
-      <a :href="`https://wa.me/${this.config.phone}?text=¡Hola!%20quiero%20más%20información%20sobre%20...`" target="_blank" class="text-secondary">
+      <a :href="`https://wa.me/52${this.config.phone}?text=¡Hola!%20quiero%20más%20información%20sobre%20...`" target="_blank" class="text-secondary">
         <i class="fab fa-whatsapp fa-2x"></i>
       </a>
       <!-- <div class="mt-3">
