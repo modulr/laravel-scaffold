c382f49fb59822852d704e5c8c0ebc0377bf9650
alfredobarron <alfredobarronc@gmail.com>
2020-04-24 16:18:50 -0500

    delete fb pixel

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -44,24 +44,6 @@
     }
     </script>
 
-    <!-- Facebook Pixel Code -->
-    <script>
-      !function(f,b,e,v,n,t,s)
-      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-      n.queue=[];t=b.createElement(e);t.async=!0;
-      t.src=v;s=b.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t,s)}(window, document,'script',
-      'https://connect.facebook.net/en_US/fbevents.js');
-      fbq('init', '1232656243581745');
-      fbq('track', 'PageView');
-    </script>
-    <noscript><img height="1" width="1" style="display:none"
-      src="https://www.facebook.com/tr?id=1232656243581745&ev=PageView&noscript=1"
-    /></noscript>
-    <!-- End Facebook Pixel Code --> 
-
     <!-- Fonts -->
     <!-- <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css"> -->
